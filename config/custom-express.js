c46686a00a32ab6b3a5227b0d66a4da3b35d46bf
Edgar Muniz Berlinck <eberlinck@mpes.mp.br>
2018-09-10 21:45:23 -0300

    Added cors support

--- config/custom-express.js
+++ config/custom-express.js
@@ -1,19 +1,15 @@
 var express = require('express');
 var consign = require('consign');
 var bodyParser = require('body-parser');
+var cors = require('cors')
 
 module.exports = function(){
     var app = express();
     var hidePoweredBy = require('hide-powered-by')
     app.use(hidePoweredBy({ setTo: 'PHP 4.2.0' }))
-
+    app.use(cors())
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
-    app.all('/', function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        next();
-    });
 
     consign()
         .include('controllers')
